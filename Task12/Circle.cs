a0b0fa3644ae82506d1176b205bcc02c711d996a
Кирилл Аксенов <kirill.ax@tprofeng.ru>
2022-02-01 14:51:14 +0300

    Добавьте файлы проекта.

---
+++ Task12/Circle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task12
+{
+    static class Circle
+    {
+        static public double GetCircumference(int r)
+        {
+            return 2 * r * Math.PI;
+        }
+        static public double GetSquare(int r)
+        {
+            return Math.Pow(r, 2) * Math.PI;
+        }
+        static public string ToBelong(int x, int y, int r)
+        {
+            double xy = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+            if (xy<=r)
+            {
+                return "Точка принадлежит кругу";
+            }
+            else
+            {
+                return "Точка не принадлежит кругу";
+            }
+               
+        }
+    }
+}
---
+++ Task12/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task12
+{
+    class Program
+    {
+        //Разработать статический класс для работы с окружностью. Класс должен содержать 3 метода:
+        //метод, определяющий длину окружности по заданному радиусу;
+        //метод, определяющий площадь круга по заданному радиусу;
+        //метод, проверяющий принадлежность точки с координатами(x, y) кругу с радиусом r и координатами центра x0, y0.
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите радиус окружности r = ");
+            int r = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите координату x = ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите координату y = ");
+            int y = Convert.ToInt32(Console.ReadLine());
+            double circumference = Circle.GetCircumference(r);
+            double square = Circle.GetSquare(r);
+            string belonging = Circle.ToBelong(x, y, r);
+            Console.WriteLine("Длина окружности равна {0}", circumference);
+            Console.WriteLine("Площадь круга равна {0}", square);
+            Console.WriteLine(belonging);
+            Console.ReadKey();
+        }
+    }
+}
